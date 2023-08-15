fd7fc2e5b93ef52e14af43630b717099e595aef9
Marcus Carvalho <135276762+mdenoak@users.noreply.github.com>
2023-08-14 23:13:45 -0300

    Create desafio 1 - tempo estimado de entrega.py
    Desafio de Python

---
+++ desafio 1 - tempo estimado de entrega.py
@@ -0,0 +1,8 @@
+
+nomeRestaurante = input()
+tempoEstimadoEntrega = int(input())
+
+def informarTempoEntregaRestaurante(nomeRestaurante, tempoEstimadoEntrega):
+    print(f"O restaurante {nomeRestaurante} entrega em {tempoEstimadoEntrega} minutos.")
+
+informarTempoEntregaRestaurante(nomeRestaurante,tempoEstimadoEntrega)
